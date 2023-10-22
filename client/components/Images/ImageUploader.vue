89615c703a4171cc1c065dde50d7aeaa42e7779d
Henry Asa <henryjacobasa@gmail.com>
2023-10-22 00:18:24 -0400

    Added `Image` uploading

---
+++ client/components/Images/ImageUploader.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAg8w-PHMXNn9Ji7bn4im8kwoBM5azV4YA",
+  authDomain: "uploading-images-b5337.firebaseapp.com",
+  projectId: "uploading-images-b5337",
+  storageBucket: "uploading-images-b5337.appspot.com",
+  messagingSenderId: "1055327096317",
+  appId: "1:1055327096317:web:8ac5991adf495316e55d94",
+  measurementId: "G-QL17FDF52X",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { defineEmits, ref as vueRef } from "vue";
+import { v4 } from "uuid";
+
+const imageUpload = vueRef();
+const imageSrc = vueRef();
+
+const emit = defineEmits(["update:imageSrc"]);
+
+initializeApp(firebaseConfig);
+
+const storage = getStorage();
+
+function handleFileChange(event: Event) {
+  if (event.target) {
+    const target = event.target as HTMLInputElement;
+    if (target?.files?.length) {
+      imageUpload.value = target.files[0];
+    }
+  }
+}
+
+const uploadImage = async () => {
+  const file = imageUpload.value as File;
+  const imageRef = ref(storage, `images/${file.name + v4()}`);
+
+  await uploadBytes(imageRef, imageUpload.value).then(async (response) => {
+    console.log(response);
+    await getDownloadURL(ref(storage, response.ref.fullPath)).then((url) => {
+      console.log("this is url");
+      imageSrc.value = url;
+      emit("update:imageSrc", url);
+    });
+  });
+};
+</script>
+
+<template>
+  <div>
+    <img v-if="imageSrc" :src="imageSrc" alt="Your Image" />
+    <br />
+    <input type="file" @change="handleFileChange" />
+    <button class="pure-button pure-button-primary" @click="uploadImage">Upload ID</button>
+    <br />
+  </div>
+</template>
+
+<style></style>
--- client/components/Login/RegisterForm.vue
+++ client/components/Login/RegisterForm.vue
@@ -2,16 +2,24 @@
 import router from "@/router";
 import { useUserStore } from "@/stores/user";
 import { ref } from "vue";
+import ImageUploader from "../Images/ImageUploader.vue";
 
+const first_name = ref("");
+const last_name = ref("");
 const username = ref("");
 const password = ref("");
+const profile_picture = ref("");
 const { createUser, loginUser, updateSession } = useUserStore();
 
 async function register() {
-  await createUser(username.value, password.value);
+  await createUser(username.value, password.value, first_name.value, last_name.value, profile_picture.value);
   await loginUser(username.value, password.value);
   void updateSession();
   void router.push({ name: "Home" });
+}
+
+function uploadImage(url: string) {
+  profile_picture.value = url;
 }
 </script>
 
@@ -20,12 +28,23 @@ async function register() {
     <h3>Register User</h3>
     <fieldset>
       <div class="pure-control-group">
+        <label for="aligned-name">First Name</label>
+        <input v-model.trim="first_name" type="text" id="aligned-name" placeholder="First Name" required />
+      </div>
+      <div class="pure-control-group">
+        <label for="aligned-name">Last Name</label>
+        <input v-model.trim="last_name" type="text" id="aligned-name" placeholder="Last Name" required />
+      </div>
+      <div class="pure-control-group">
         <label for="aligned-name">Username</label>
         <input v-model.trim="username" type="text" id="aligned-name" placeholder="Username" required />
       </div>
       <div class="pure-control-group">
         <label for="aligned-password">Password</label>
         <input type="password" v-model.trim="password" id="aligned-password" placeholder="Password" required />
+      </div>
+      <div class="pure-control-group">
+        <ImageUploader @update:imageSrc="uploadImage"></ImageUploader>
       </div>
       <div class="pure-controls">
         <button type="submit" class="pure-button pure-button-primary">Register</button>
