1d2373462ec602281b902febeecc4c2f6bc6b215
Henry Asa <henryjacobasa@gmail.com>
2023-11-01 21:03:05 -0400

    Implemented the `ActivityView`

--- client/views/ActivityView.vue
+++ client/views/ActivityView.vue
@@ -5,7 +5,7 @@ import { storeToRefs } from "pinia";
 import { onBeforeMount, onUpdated, ref } from "vue";
 import { fetchy } from "../utils/fetchy";
 
-const { isLoggedIn } = storeToRefs(useUserStore());
+const { isLoggedIn, currentUsername } = storeToRefs(useUserStore());
 
 const props = defineProps(["name"]);
 const loaded = ref(false);
@@ -30,21 +30,42 @@ onBeforeMount(async () => {
 onUpdated(async () => {
   await getActivity();
 });
+
+function isMember() {
+  const usernames = activityGroup.value.members.map((member: UserDoc) => member.username);
+  return usernames.includes(currentUsername.value);
+}
+
+function isManager() {
+  const usernames = activityGroup.value.managers.map((manager: UserDoc) => manager.username);
+  return usernames.includes(currentUsername.value);
+}
 </script>
 
 <template>
-  <!-- <h1>
-    Welcome to the <span class="gradient-text">{{ props.name }}</span> Activity!
-  </h1> -->
-  <p>Hello!</p>
-  <p>isLoggedIn: {{ isLoggedIn }}</p>
-  <p>Loaded: {{ loaded }}</p>
-  <section class="activities">
-    <article>
-      <ActivityComponent v-if="isLoggedIn && loaded" :activity="activityGroup" @refreshActivities="onUpdated" />
-      <!-- <EditActivityForm v-else :activity="activity" @refreshActivities="getActivities" @editActivity="updateEditing" /> -->
-    </article>
-  </section>
+  <div v-if="isMember()">
+    <div class="top-heading">
+      <h1>
+        Welcome to the <span class="gradient-text">{{ props.name }}</span> Activity!
+      </h1>
+      <h2 v-if="isManager()">You are a manager of this activity. As such, you are granted extra privileges and functionality within the group.</h2>
+      <h2>
+        <span class="gradient-text">{{ props.name }}</span> is located at {{ activityGroup.location }}.
+      </h2>
+    </div>
+    <section class="activities">
+      <article>
+        <ActivityComponent v-if="isLoggedIn && loaded" :activity="activityGroup" @refreshActivities="onUpdated" />
+        <!-- <EditActivityForm v-else :activity="activity" @refreshActivities="getActivities" @editActivity="updateEditing" /> -->
+      </article>
+    </section>
+  </div>
+  <div v-else class="top-heading">
+    <h2>
+      You are not a member of the <span class="gradient-text">{{ props.name }}</span> activity.
+    </h2>
+    <ActivityComponent v-if="isLoggedIn && loaded" :activity="activityGroup" @refreshActivities="onUpdated" />
+  </div>
 </template>
 
 <style scoped>
@@ -52,6 +73,11 @@ onUpdated(async () => {
   background: -webkit-linear-gradient(0deg, red, orange);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+}
+
+.top-heading {
+  padding-left: 5em;
+  padding-right: 5em;
 }
 
 section {
